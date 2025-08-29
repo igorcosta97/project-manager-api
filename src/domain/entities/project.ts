cd8f2ce90aac3d823022499c112ee59ea14314ea
Igor Silva <igor.silva@neppo.com.br>
2025-08-29 19:05:33 -0300

    assignment-2: create models, infra, use-cases and controllers

--- src/domain/entities/project.ts
+++ src/domain/entities/project.ts
@@ -8,24 +8,4 @@ export class Project implements IProject {
   description: string;
   tasks: ITask[];
   user: IUser;
-
-  constructor(id: number, name: string, description: string, tasks: ITask[], user: IUser) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.tasks = tasks;
-    this.user = user;
-  }
-
-  addTask(task: ITask): void {
-    this.tasks.push(task);
-  }
-
-  removeTask(taskId: number): void {
-    this.tasks = this.tasks.filter(task => task.id !== taskId);
-  }
-
-  getTask(taskId: number): ITask | undefined {
-    return this.tasks.find(task => task.id === taskId);
-  }
 }
--- src/domain/entities/task.ts
+++ src/domain/entities/task.ts
@@ -8,26 +8,4 @@ export class Task implements ITask {
   status: 'pending' | 'completed';
   project: IProject;
   user: IUser;
-
-  constructor(
-    id: number,
-    name: string,
-    status: 'pending' | 'completed',
-    project: IProject,
-    user: IUser,
-  ) {
-    this.id = id;
-    this.name = name;
-    this.status = status;
-    this.project = project;
-    this.user = user;
-  }
-
-  complete(): void {
-    this.status = 'completed';
-  }
-
-  reopen(): void {
-    this.status = 'pending';
-  }
 }
